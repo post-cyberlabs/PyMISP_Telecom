b937e4442b3e42751c6e3a09dfd9f4f177e17054
Alexandre De Oliveira <mail@alexandredeoliveira.fr>
2021-01-08 16:51:36 +0100

    Add the template for keys file

---
+++ keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+misp_url = 'https://misp_url_here'
+misp_key = '' # The MISP auth key can be found on the MISP web interface under the menu "Global Action > My Profile > Authkey"
+misp_verifycert = True
+misp_client_cert = ''
+proofpoint_key = ''
