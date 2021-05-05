1bfe87b082bf38788b402c254a07ec7ab8bf34bb
Alexandre De Oliveira <yodresh@gmail.com>
2021-05-05 09:51:11 +0200

    Update to remove artifacts from extraction

--- extract_url_820.py
+++ extract_url_820.py
@@ -3,24 +3,22 @@
 
 from pymisp import ExpandedPyMISP
 from keys import misp_url, misp_key, misp_verifycert
-from phone_iso3166.country import *
 
-# This script is a really simple example on how to extract call spam numbers in Telecom MISP events
-# I then create a file with the phone numbers
+# This script is a really simple example on how to extract FluBot URL published on event 820
+# It will create a urls.txt file
 
-f.write('number,country_iso2\n')
+f = open('urls.txt','w')
 
 if __name__ == '__main__':
 
     misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert)
     result = misp.search(eventid=820, return_format='json')
 
-    f = open('urls.txt','w')
-
     for r in result:
         for o in r['Event']['Object']:
             for a in o['Attribute']:
                 if a['object_relation'] == 'url':
-                    f.write(a['value'])
-                    f.write('\n')
+                    if '//' in a['value']:
+                        f.write(a['value'])
+                        f.write('\n')
     f.close()
