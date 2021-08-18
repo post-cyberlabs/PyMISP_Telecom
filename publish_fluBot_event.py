edbf0e01ddc64ee593a9b9e7c23faf6b223fda78
Alexandre De Oliveira <yodresh@gmail.com>
2021-08-18 15:28:13 +0200

    Remove forgotten debug print

--- publish_fluBot_event.py
+++ publish_fluBot_event.py
@@ -33,7 +33,6 @@ event.set_date(d)
 with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        print(row)
         misp_object = MISPObject('url', standalone=False)
         misp_object.comment = 'Flubot Domain detect via TIDS'
         misp_object.add_attribute('url', value=row[0])
